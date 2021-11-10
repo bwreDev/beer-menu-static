c4f042b11a4b1af4813f198569994179aeb1782e
cbonner26118 <cbonner26118@gmail.com>
2021-11-10 15:20:00 -0800

    Added banner to link Untappd account.

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,55 @@
 import Copyright from '../components/Copyright';
 import TapList from '../components/TapList';
 import Head from 'next/head';
+import Banner from '../components/Banner';
 
 export default function Home({ taps }) {
-  return (
-    <>
-      <Head>
-        <title>Self-Pour Taps</title>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/site.webmanifest' />
-      </Head>
-      <TapList taps={taps} />
-      <Copyright />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Blast 825 Orcutt | Self-Pour Taps</title>
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+			</Head>
+			<Banner />
+			<TapList taps={taps} />
+			<Copyright />
+		</>
+	);
 }
 
 export async function getStaticProps() {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const settings = {
-    headers: {
-      Authorization: `Basic ${apiKey}`,
-    },
-  };
-  const res = await fetch(
-    `https://business.untappd.com/api/v1/sections/669648/items`,
-    settings
-  );
-  const taps = await res.json();
+	const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+	const settings = {
+		headers: {
+			Authorization: `Basic ${apiKey}`,
+		},
+	};
+	const res = await fetch(
+		`https://business.untappd.com/api/v1/sections/669648/items`,
+		settings
+	);
+	const taps = await res.json();
 
-  return {
-    props: {
-      taps,
-    },
-  };
+	return {
+		props: {
+			taps,
+		},
+	};
 }
