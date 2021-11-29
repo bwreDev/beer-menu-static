4a95b2c7cf0a70484efda3c27c9463e41c7400d5
cbonner26118 <cbonner26118@gmail.com>
2021-11-28 18:37:56 -0800

    ;aldkjf

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({ taps }) {
 	);
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
 	const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 	const settings = {
 		headers: {
